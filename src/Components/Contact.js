683fce2cbcebf90d8e4b21a6c6cf584168614325
AkshayGopal0704 <112542222+AkshayGopal0704@users.noreply.github.com>
2022-08-31 13:42:52 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -30,7 +30,7 @@ export default function Contact() {
     </div>
     </div>
     <div className='col-6'> 
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251482.44857921428!2d76.16084508763808!3d9.98268091865779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080d514abec6bf%3A0xbd582caa5844192!2sKochi%2C%20Kerala!5e0!3m2!1sen!2sin!4v1661324809355!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe title="location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251482.44857921428!2d76.16084508763808!3d9.98268091865779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080d514abec6bf%3A0xbd582caa5844192!2sKochi%2C%20Kerala!5e0!3m2!1sen!2sin!4v1661324809355!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
    </div>
   
